d716222429244194d30ee4e4e2df37ce6f1666de
Voctor <aivictor@mpcircle.org>
2025-05-25 22:19:07 +0100

    Removed unnecesarry files from by adding them to .gitignore

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+CLS#!/bin/bash
 
 # Start Ollama server in the background
 echo "Starting Ollama server..."
@@ -13,4 +13,4 @@ echo "Ollama server started. Proceeding with initialization..."
 python scripts/initialize.py
 
 # Start Streamlit
-streamlit run src/app.py --server.port=8501 --server.address=0.0.0.0
+streamlit run src/app.py --server.port=8501 --server.address=0.0.0.0 
