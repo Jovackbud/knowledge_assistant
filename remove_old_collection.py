3d38035ecc5ab8516ff0224ccc9de242e7174609
Voctor <aivictor@mpcircle.org>
2025-06-03 10:03:38 +0100

    Removed files I dont need

--- remove_old_collection.py
+++
@@ -1,7 +0,0 @@
-from pymilvus import connections, utility
-
-# connect to your Milvus
-connections.connect(alias="default", host="localhost", port="19530")
-
-# drop the old collection and all its data
-utility.drop_collection("adv_rbac_kb_v1")
